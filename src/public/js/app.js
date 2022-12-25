fb0bb7d65cdbd25565253f6bc520ea588b1baa8c
chan <chan.jor.zz@gmail.com>
2022-12-26 06:58:46 +0900

    socket on message send and connecting browser and server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,31 @@
 // alert("hello!");
 // console.log("hello");
 
+// 새로고침할 때 작동함
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 // function fu(event) {
 // }
 // btn.addEventListener('click', fn)
+
+// socket이 connection을 open 했을 때 발생하는 event
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+// 서버로부터 메시지를 받을 때 message 출력
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+// server와 연결이 끊어졌을 때
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+// FE > BE로 메세지 보내기
+// 1. 메시지 보내기
+// 즉시 실행되길 원하지 않기 때문에 timeout을 사용함
+setTimeout(() => {
+  socket.send("hello from the browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -16,9 +16,27 @@ const handleListen = () => console.log(`Listening on http://localhost:4000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+// function handleConnection(socket) {
+//   console.log(socket);
+// }
+// wss.on("connection", handleConnection);
+
+// 연결해서 소켓을 알 수 있음
+wss.on("connection", (socket) => {
+  // console.log(socket);
+  // 브라우저가 연결 되었을 때
+  console.log("Connected to Browser ✅");
+  // 브라우저가 꺼졌을 때
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  // 브라우저가 서버에 메세지를 보냈을 때
+  socket.on("message", (message) => {
+    // message만 보냈을 경우
+    // <Buffer 68 65 6c 6c 6f 20 66 72 6f 6d 20 74 68 65 20 62 72 6f 77 73 65 72> 출력됨
+    // toString('utf8') 인코딩해야 함
+    console.log(message.toString("utf8"));
+  });
+  // socket으로 데이터를 보냄 (브라우저가 메세지를 보내기)
+  socket.send("hello!!!");
+});
 
 server.listen(4000, handleListen);
