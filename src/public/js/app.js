b0f07304694772a88997a531b84799c559df4029
chan <chan.jor.zz@gmail.com>
2022-12-26 17:18:53 +0900

    nickname and message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,11 +2,16 @@
 // console.log("hello");
 
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
 // 새로고침할 때 작동함
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 // function fu(event) {
 // }
 // btn.addEventListener('click', fn)
@@ -18,7 +23,12 @@ socket.addEventListener("open", () => {
 
 // 서버로부터 메시지를 받을 때 message 출력
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  //   console.log("New message: ", message.data);
+
+  // 새로운 메시지를 받으면 ul안에 li를 만들어서 추가
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // server와 연결이 끊어졌을 때
@@ -37,11 +47,28 @@ function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
   // FE의 form에서 BE로 무언가를 보냄
-  socket.send(input.value);
+  //   socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   // input에 메시지를 입력하고 send버튼을 누르면 콘솔에 출력됨
   // console.log(input.value);
   // send 후 input값 비우기
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  //   socket.send(input.value);
+
+  // JSON 형식
+  //   socket.send({
+  //     type: "nickname",
+  //     payload: input.value,
+  //   });
+
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -28,20 +28,44 @@ const sockets = [];
 wss.on("connection", (socket) => {
   // 다른 곳에서 연결되었을 때 다른곳의 socket을 sockets array에 넣어줌
   sockets.push(socket);
+  // 익명일 경우
+  socket["nickname"] = "Anon";
   // console.log(socket);
   // 브라우저가 연결 되었을 때 (채팅방에 들어갔을 때 - 버튼을 누르는 것)
   console.log("Connected to Browser ✅");
   // 브라우저가 꺼졌을 때
   socket.on("close", () => console.log("Disconnected from Browser ❌"));
   // 브라우저가 서버에 메세지를 보냈을 때
-  socket.on("message", (message) => {
+  socket.on("message", (msg) => {
+    // console.log(message.toString("utf8"));
+    const message = JSON.parse(msg);
+    // console.log(parsed, message.toString("utf8"));
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        // console.log(message.payload);
+        socket["nickname"] = message.payload;
+        break;
+    }
+
+    /*
+    if (parsed.type === "new_message") {
+      // 각 브라우저를 aSocket으로 표시하고 메세지를 보낸다는 의미
+      // sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+      sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+    } else if (parsed.type === "nickname") {
+      console.log(parsed.payload);
+    }
+    */
+
     // message만 보냈을 경우
     // <Buffer 68 65 6c 6c 6f 20 66 72 6f 6d 20 74 68 65 20 62 72 6f 77 73 65 72> 출력됨
     // toString('utf8') 인코딩해야 함
     // console.log(message.toString("utf8"));
-
-    // 각 브라우저를 aSocket으로 표시하고 메세지를 보낸다는 의미
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
 
     // FE에서 BE로 보낸 메세지를 다시 보내줌
     // socket.send(message.toString("utf8"));
@@ -51,3 +75,13 @@ wss.on("connection", (socket) => {
 });
 
 server.listen(4000, handleListen);
+
+// {
+//     type: 'message',
+//     payload: "hello everyone!"
+// }
+
+// {
+//     type: "nickname",
+//     payload: "chan"
+// }
